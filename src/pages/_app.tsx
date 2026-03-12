eec6aad5840c12a0d3269d15ee944c6d1d513e94
jotavitorz <joaovitorxz812@gmail.com>
2026-03-12 10:11:03 -0300

    feat: criar componente Header e adicionar estilizacao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Header } from "../components/header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Header />
       <Component {...pageProps} />
     </>
   );
