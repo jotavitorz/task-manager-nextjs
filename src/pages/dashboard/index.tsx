b1f379977625506003bc3618104b2f2eab8660ce
jotavitorz <joaovitorxz812@gmail.com>
2026-03-17 10:56:38 -0300

    feat: adicionar estrutura inicial da pagina Dashboard

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Dashboard() {
+    return (
+        <div>
+            <Head>
+                <title>Meu painel de tarefas</title>
+            </Head>
+
+            <h1>Pagina Painel</h1>
+        </div>
+    )
+}
