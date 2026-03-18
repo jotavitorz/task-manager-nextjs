f99c2c1682af025c0e72abce5b13a5854c10b974
jotavitorz <joaovitorxz812@gmail.com>
2026-03-18 10:06:27 -0300

    adicionar estrutura inicial da Dashboard com criacao de tarefas e componente Textarea reutilizavel

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,16 +1,41 @@
 import { GetServerSideProps } from "next";
+import styles from "./styles.module.css";
 import Head from "next/head";
 
 import { getSession } from "next-auth/react";
+import { Textarea } from "../../components/textarea";
 
 export default function Dashboard() {
     return (
-        <div>
+        <div className={styles.container}>
             <Head>
                 <title>Meu painel de tarefas</title>
             </Head>
 
-            <h1>Pagina Painel</h1>
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        <h1 className={styles.title}>Qual sua tarefa?</h1>
+
+                        <form>
+                            <Textarea
+                                placeholder="Digite qual sua tarefa"
+                            />
+                            <div className={styles.checkboxArea}>
+                                <input 
+                                    type="checkbox" 
+                                    className={styles.checkbox}
+                                />
+                                <label htmlFor="">Deixar tarefa publica?</label>
+                            </div>
+
+                            <button className={styles.button} type="submit">
+                                Registrar
+                            </button>
+                        </form>
+                    </div>
+                </section>
+            </main>
         </div>
     )
 }
