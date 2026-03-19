25cab3ea7f6c6c3e35c5317b1a97675145092a42
jotavitorz <joaovitorxz812@gmail.com>
2026-03-19 09:39:28 -0300

    implementar secao de listagem de tarefas (estatica) no Dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,10 @@
 import { GetServerSideProps } from "next";
 import styles from "./styles.module.css";
 import Head from "next/head";
-
 import { getSession } from "next-auth/react";
 import { Textarea } from "../../components/textarea";
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash } from "react-icons/fa";
 
 export default function Dashboard() {
     return (
@@ -34,6 +35,33 @@ export default function Dashboard() {
                             </button>
                         </form>
                     </div>
+                </section>
+
+                <section className={styles.taskContainer}>
+                    <h1>Minhas tarefas</h1>
+
+                    <article className={styles.task}>
+
+                        <div className={styles.tagContainer}>
+                            <label className={styles.tag}>PUBLICO</label>
+                            <button className={styles.shareButton}>
+                            <FiShare2
+                                size={22}
+                                color="#3183ff"
+                            />
+                            </button>
+                        </div>
+
+                        <div className={styles.taskContent}>
+                            <p>Minha primeira tarefa de exemplo show damis!</p>
+                            <button className={styles.trashButton}>
+                                <FaTrash 
+                                    size={24}
+                                    color="#ea3140"
+                                />
+                            </button>
+                        </div>
+                    </article>
                 </section>
             </main>
         </div>
