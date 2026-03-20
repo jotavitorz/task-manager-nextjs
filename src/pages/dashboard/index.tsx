3a606780ee88b3b4be62ed1b775fe1e5afd38990
jotavitorz <joaovitorxz812@gmail.com>
2026-03-20 10:08:49 -0300

    feat: integrar criacao de tarefas com o Firebase

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,8 +6,16 @@ import { getSession } from "next-auth/react";
 import { Textarea } from "../../components/textarea";
 import { FiShare2 } from "react-icons/fi";
 import { FaTrash } from "react-icons/fa";
+import { db } from "../../services/firebaseConnection";
+import { addDoc, collection } from "firebase/firestore";
 
-export default function Dashboard() {
+interface HomeProps {
+    user: {
+        email: string;
+    }
+}
+
+export default function Dashboard({ user }: HomeProps) {
     const [input, setInput] = useState("");
     const [publicTask, setPublicTask] = useState(false);
 
@@ -15,12 +23,25 @@ export default function Dashboard() {
         setPublicTask(event.target.checked)
     }
 
-    function handleRegisterTask(event: FormEvent) {
+    async function handleRegisterTask(event: FormEvent) {
         event.preventDefault();
 
         if(input === "") return;
 
-        alert("teste");
+        try {
+            const dbRef = collection(db, "tarefas");
+            await addDoc(dbRef, {
+                tarefa: input,
+                created: new Date(),
+                user: user.email,
+                public: publicTask,
+            });
+
+            setInput("")
+            setPublicTask(false);
+        }catch(err){
+            console.log(err);
+        }
     }
 
     return (
@@ -101,6 +122,10 @@ export const getServerSideProps: GetServerSideProps = async ({req}) => {
     }
 
     return {
-        props: {},
+        props: {
+            user: {
+                email: session.user.email,
+            }
+        },
     }
 }
